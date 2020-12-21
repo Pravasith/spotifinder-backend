8fdb8f7c7dee9ff9c8a902a9014728694364e4ff
Pravas Chinthoju <pravasith@gmail.com>
2020-12-21 21:42:00 +0530

    Trying GraphQL now

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,21 +1,16 @@
-import { fetchOptions, fetchData } from './../libs/hitAPIs';
+
+import { fetchOptions, fetchData } from './../libs/hitAPIs'
 import configs from '../configs'
+import { GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql'
 
 const { accessToken } = configs.spotify
 
 
 
-const url = 'https://api.spotify.com/v1/search?q=roadhouse%20blues&type=album,track&limit=3'
-const options: fetchOptions = {
-    method: 'get',
-    headers: { 
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Bearer ' + accessToken
-    },
-}
 
 
-export const searchData = <T>(): Promise<T> => {
+
+export const searchData = <T>(url: string, options: fetchOptions): Promise<T> => {
     return new Promise((resolve, reject) => {
         fetchData(url, options)
         .then(data => resolve(data as Promise<T>))
@@ -23,7 +18,58 @@ export const searchData = <T>(): Promise<T> => {
             console.error(err)
             reject(err)
         })
-
     })
 }
+
+
+
+const TrackType = new GraphQLObjectType({
+    name: 'tracks',
+    description: '...',
+
+    fields: () => ({
+        name: {
+            type: GraphQLString,
+            resolve: json => {
+                console.log(json.tracks.items)
+                return json.tracks.items.uri
+            }
+        },
+        
+    })
+})
+
+
+export default new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'query',
+        description: '...',
+
+        fields: () => ({
+            tracks: {
+                type: TrackType,
+                args: {
+                    name: {
+                        type: GraphQLString
+                    }
+                },
+                resolve: (root, args) => {
+                    const url = `https://api.spotify.com/v1/search?q=${ args.name }&type=track&limit=3`
+                    const options: fetchOptions = {
+                        method: 'get',
+                        headers: { 
+                            'Content-Type': 'application/x-www-form-urlencoded',
+                            'Authorization': 'Bearer ' + accessToken
+                        },
+                    }
+
+                    root
+                    return searchData(url, options)
+
+
+                }
+            }
+        })
+    })
+})
 
--- src/serve.ts
+++ src/serve.ts
@@ -1,31 +1,26 @@
-import { searchData } from './schema/index';
-
 import * as express from 'express'
 // import { accessTokenData } from './libs/setToken'
 
 
-// import { graphqlHTTP } from 'express-graphql'
-// import { schema } from './schema'
+import { graphqlHTTP } from 'express-graphql'
+import schema from './schema'
 
 
 const main = async () => {
     const app = express()
 
-
-    let x: any = await searchData()
-
-    console.log(x.albums.items)
-
     // console.log(await accessTokenData() ) // Do this to get it when token and cookie expire
 
 
-    // app.use('/graphql', graphqlHTTP({
-    //     schema,
-    //     graphiql: true
-    // }))
+    app.use('/graphql', graphqlHTTP({
+        schema,
+        graphiql: true
+    }))
+
+    app.listen(4000, () => {
+        console.log('listening... ')
+    })
     
-    app.listen(4000)
-    console.log('listening... ')
 }
 
 main()
