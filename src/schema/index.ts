3fee597fb5656cb6945e83b6550be25311c89919
Pravas Chinthoju <pravasith@gmail.com>
2020-12-23 14:04:04 +0530

    GraphQL finally works

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,7 +1,14 @@
 
 import { fetchOptions, fetchData } from './../libs/hitAPIs'
 import configs from '../configs'
-import { GraphQLID, GraphQLInt, GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql'
+import { 
+    GraphQLID, 
+    GraphQLInt, 
+    GraphQLList,
+    GraphQLObjectType, 
+    GraphQLSchema, 
+    GraphQLString,
+} from 'graphql'
 
 const { accessToken } = configs.spotify
 
@@ -23,35 +30,91 @@ export const searchData = <T>(url: string, options: fetchOptions): Promise<T> =>
 
 
 
+const ImageType = new GraphQLObjectType({
+    name: 'Image',
+    fields: {
+        height: { type: GraphQLInt },
+        width: { type: GraphQLInt },
+        url: { type: GraphQLString },
+    }
+})
+
+
 const TrackType = new GraphQLObjectType({
-    name: 'tracks',
-    description: '...',
+    name: 'Track',
+    description: 'Gets the track object from spotify api.',
+    fields: () => ({
+        album: {
+            type: new GraphQLObjectType({
+                name: 'album',
+                fields: {
+                    name: { type: GraphQLString },
+                    id: { type: GraphQLString },
+                    images: { type: new GraphQLList(ImageType) },
+                    uri: { type: GraphQLString },
+                    href: { type: GraphQLString }
+                }
+            })
+        },
+        preview_url: { type: GraphQLString },
+        name: { type: GraphQLString },
+        id: { type: GraphQLID },
+        artists: { type: new GraphQLList(GraphQLString) },
+
+        // Optional
+        popularity: { type: GraphQLInt },
+        type: { type: GraphQLString },
+        available_markets: { type: new GraphQLList(GraphQLString) },
+        duration_ms: { type: GraphQLInt },
+        uri: { type: GraphQLString },
+        href: { type: GraphQLString },
+    })
+})
+
+
+
+
+const SearchResultType = new GraphQLObjectType({
+    name: 'SearchQueryResponse',
+    description: "Returns the albums, artists, and tracks that match the user's query in the request",
 
     fields: () => ({
-        name: { type: GraphQLString },
-        popularity: { type: GraphQLInt },
-        preview_url: { type: GraphQLString },
-        duration_ms: { type: GraphQLInt },
-        id: { type: GraphQLID }
+        tracks: {
+            type: new GraphQLList(TrackType),
+            resolve: (root: any) => {
+                console.log(root)
+                return root
+            }
+        },
+        // albums: {
+        //     type: new GraphQLList(AlbumType)
+        // },
+        // artist: {
+        //     type: new GraphQLList(ArtistType)
+        // }
+
+        
     })
 })
 
 
 export default new GraphQLSchema({
     query: new GraphQLObjectType({
-        name: 'query',
+        name: 'RootQuery',
         description: '...',
 
-        fields: () => ({
-            tracks: {
-                type: TrackType,
+        fields: {
+
+            // Search Query results
+            search_results: {
+                type: new GraphQLList(SearchResultType),
                 args: {
-                    name: {
+                    query: {
                         type: GraphQLString
                     }
                 },
                 resolve: async (root, args) => {
-                    const url = `https://api.spotify.com/v1/search?q=${ args.name }&type=track,artist&limit=3`
+                    const url = `https://api.spotify.com/v1/search?q=${ args.query }&type=track&limit=2`
                     const options: fetchOptions = {
                         method: 'get',
                         headers: { 
@@ -62,15 +125,58 @@ export default new GraphQLSchema({
 
                     const data: any = await searchData(url, options)
 
-                    // let { items } = data.tracks
-                    console.log(data)
-                    // let {}
+                    let tracks = data.tracks.items
+
+                    tracks = tracks.map((item: any) => {
+
+                        const {
+                            album,
+                            preview_url,
+                            name,
+                            id,
+                            artists,
+                            popularity,
+                            type,
+                            available_markets,
+                            duration_ms,
+                            uri,
+                            href,
+                        } = item
+
+                        return {
+                            album: {
+                                // Mandatory
+                                name: album.name,
+                                id: album.id,
+                                images: album.images,
+    
+                                // Optional
+                                uri: album.uri,
+                                href: album.href,
+                            },
+                            preview_url,
+                            name,
+                            id,
+                            artists: artists.map((artist: any) => artist.name),
+                        
+                            // Optional
+                            popularity,
+                            type,
+                            available_markets,
+                            duration_ms,
+                            uri,
+                            href,
+                        }
+                        
+                    })
+
                     root
-                    return data.tracks.items[0]
+                    return [tracks]
 
                 }
             }
-        })
+        }
+
     })
 })
 
